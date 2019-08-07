9c0bf1503b4ba4fba606ae34801b76c3d0185758
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-07 13:10:57 +1000

    initial

---
+++ Assets/Tests/TestSuite.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.TestTools;
+using NUnit.Framework;
+public class TestSuite 
+{
+    public GameObject game;
+    private GameManager gameManager;
+    private Player player;
+    [SetUp]
+   public void  Setup()
+    {
+       
+        //load and spawn game prefab
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/Game");
+         game = Object.Instantiate(prefab);
+        //get gamemanager
+        gameManager = GameManager.Instance;
+        //get Player
+        player = game.GetComponentInChildren<Player>();
+    }
+
+    [UnityTest]
+   public IEnumerator GamePrefabLoaded()
+    {
+        yield return new WaitForEndOfFrame();
+
+        //after we wait
+        Assert.NotNull(game, "Make sure game prefab is working");
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerExists()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Assert.NotNull(player, "Player dont exist");
+    }
+
+
+    [TearDown]
+   public  void Teardown()
+    {
+        Object.Destroy(game);
+    }
+
+}
